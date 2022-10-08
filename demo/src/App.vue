8aeb651078ae3c5578ffda7a2c97735be8986319
yangjinjun3 <yangjinjun3@jd.com>
2022-10-08 22:12:34 +0800

    feat: 清除浮动&指定背景图像的大小

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -2,50 +2,28 @@
 </script>
 
 <template>
-  <div class="three-dots1">
-    <div class="mask"></div>
-    <div class="mask-text">11</div>
+  <div class="three-dots1 clearfix">
+    <div class="abc">1111</div>
   </div>
 </template>
 
 <style lang="scss">
-@mixin image-mask($opacity: 0.7, $shape: circle, $zIndex: 1) {
-    position: relative;
+@mixin clearfix {
+  &::after{
+    content:"";
+    display:table;
+    clear:both;
+  }
+}
 
-    > .mask,
-    > .mask-text {
-        position: absolute;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        @if($shape == 'circle') {
-            top: 0;
-        }
-    }
-
-    > .mask {
-        z-index: $zIndex;
-        background-color: #000;
-        opacity: #{$opacity};
-        
-        @if($shape == 'circle') {
-            border-radius: 50%;
-        }
-    }
-
-    > .mask-text {
-        z-index: $zIndex + 1;
-        color: #fff;
-        word-break: break-all;
-
-        @include center;
-    }
+.clearfix {
+  @include clearfix;
 }
 
 .three-dots1 {
-  width: 100px;
-  height: 100px;
-
-  @include image-mask(0.8);
+  border:1px solid red;
+}
+.abc {
+  float: left;
 }
 </style>
