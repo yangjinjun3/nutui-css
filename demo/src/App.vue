c19e963218f295f62b7404bd7a265bb15baa5720
yangjinjun3 <yangjinjun3@jd.com>
2022-10-08 20:41:20 +0800

    重构

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -2,11 +2,50 @@
 </script>
 
 <template>
-  <div class="ymh">afdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafdaafda</div>
+  <div class="three-dots1">
+    <div class="mask"></div>
+    <div class="mask-text">11</div>
+  </div>
 </template>
 
 <style lang="scss">
-.ymh {
-  @include text-overflow('100px', '-webkit-box', 3);
+@mixin image-mask($opacity: 0.7, $shape: circle, $zIndex: 1) {
+    position: relative;
+
+    > .mask,
+    > .mask-text {
+        position: absolute;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        @if($shape == 'circle') {
+            top: 0;
+        }
+    }
+
+    > .mask {
+        z-index: $zIndex;
+        background-color: #000;
+        opacity: #{$opacity};
+        
+        @if($shape == 'circle') {
+            border-radius: 50%;
+        }
+    }
+
+    > .mask-text {
+        z-index: $zIndex + 1;
+        color: #fff;
+        word-break: break-all;
+
+        @include center;
+    }
+}
+
+.three-dots1 {
+  width: 100px;
+  height: 100px;
+
+  @include image-mask(0.8);
 }
 </style>
