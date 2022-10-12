4553ffbe7241284a1050d1947c8300012e1ca55b
yangjinjun3 <yangjinjun3@jd.com>
2022-10-12 20:02:24 +0800

    重建demo项目

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -1,28 +1,20 @@
 <script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-    <div class="abc">111111111111111111111111111111111111</div>
+  <div class="abc">1212121212121212121212121212121212121212</div>
 </template>
 
-<style lang="scss">
-@mixin text-overflow($line:1, $width:'') {
-    width: if(type-of($width) == 'number', #{$width} + 'px', #{$width});
-    overflow: hidden;
-
-    @if($line == 1) {
-      white-space: nowrap;
-      text-overflow: ellipsis;
-    }
-    @if($line > 1) {
-      display: -webkit-box;
-      -webkit-box-orient: vertical;
-      -webkit-line-clamp: $line;
-      word-break: break-all;
-    }
+<style scoped lang="scss">
+@mixin text-overflow() {
+  overflow: hidden;
 }
 
 .abc {
-  @include text-overflow(1, '100px')
+  width: 100px;
+  @include text-overflow;
 }
 </style>
--- demo/vite.config.js
+++ demo/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
 	css: {
 		preprocessorOptions: {
 			scss: {
-				additionalData: `@import "../styles/index.scss";`
+				additionalData: `@import "node_modules/@jd/nutui-css/styles/all.scss";`
 			}
 		}
 	}
